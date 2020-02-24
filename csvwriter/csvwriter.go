2fcbc2f1cf0eb994b0da650d3eaff64ce6106922
Damilare Durojaye <oluwadamilare.durojaye@trivago.com>
2020-02-24 11:13:45 +0100

    Implemented a csv writer

---
+++ csvwriter/csvwriter.go
@@ -0,0 +1,69 @@
+package csvwriter
+
+import (
+	"encoding/csv"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func Write(csvData [][]string) {
+	recordFile, err := os.Create("./superhero.csv")
+	if err != nil {
+		fmt.Println("Error creating file", err)
+		return
+	}
+
+	writer := csv.NewWriter(recordFile)
+
+	err = writer.WriteAll(csvData)
+
+	if err != nil {
+		fmt.Println("Error while writing to the file ::", err)
+		return
+	}
+
+	err = recordFile.Close()
+	if err != nil {
+		fmt.Println("Error while closing the file ::", err)
+		return
+	}
+}
+
+func WriteLine(csvData [][]string) {
+	recordFile, err := os.Create("./superheros2.csv")
+
+	if err != nil {
+		fmt.Println("Error Creating file", err)
+		return
+	}
+
+	writer := csv.NewWriter(recordFile)
+
+	for i, csvLineData := range csvData {
+		if i == 0 {
+			csvLineData = append(csvLineData, "Name Length")
+		} else {
+			nameLength := strconv.Itoa(len(csvLineData[0]))
+			csvLineData = append(csvLineData, nameLength)
+		}
+		err = writer.Write(append(csvLineData, string(i)))
+		if err != nil {
+			fmt.Println("Error while writing to the file ::", err)
+			return
+		}
+
+		writer.Flush()       // Writes the buffered data to the writer
+		err = writer.Error() // Checks if any error occurred while writing
+		if err != nil {
+			fmt.Println("Error while writing to the file ::", err)
+			return
+		}
+	}
+
+	err = recordFile.Close()
+	if err != nil {
+		fmt.Println("Error while closing the file ::", err)
+		return
+	}
+}
--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"fmt"
+	"github.com/babadee08/csvreader/csvwriter"
+)
+
+var csvData = [][]string{
+	{"SuperHero Name", "Power", "Weakness"},
+	{"Batman", "Wealth", "Human"},
+	{"Superman", "Strength", "Kryptonite"},
+}
+
 func main() {
-	
+	fmt.Println("CSV Reader sample")
+
+	csvwriter.Write(csvData)
 }
